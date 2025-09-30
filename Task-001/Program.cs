4ddc86917a4e77c5a272c9c1798f7366b19ba3d6
Mohamed Kamel <mohamed.kamel20@outlook.com>
2025-09-30 22:46:52 +0300

    Add project files.

---
+++ Task-001/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Please Insert Number Of Small Carpets : ");
+double smallCarpet = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please Insert Number Of Large Carpets : ");
+double largeCarpet = Convert.ToDouble(Console.ReadLine());
+double smallPrice = Convert.ToDouble(25);
+Console.WriteLine($"Price Per Small Carpet :${smallPrice}");
+double largePrice = Convert.ToDouble(35);
+Console.WriteLine($"Price Per Large Carpet :${largePrice}");
+double Cost = (smallCarpet * smallPrice) + (largeCarpet * largePrice);
+Console.WriteLine($"Cost :${Cost}");
+double Tax = Cost *.06;
+Console.WriteLine($"Tax :${Tax}");
+Console.WriteLine("=============================");
+decimal totalEstimate = Convert.ToDecimal(Cost) + Convert.ToDecimal(Tax);
+Console.WriteLine($"Total estimate : ${totalEstimate}");
+Console.WriteLine("this Estimate is Valid For 30 Day");
+
+
+
+
